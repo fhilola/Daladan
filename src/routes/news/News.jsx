7d2db91a93a339f436e419a1b91566c405990164
fhilola <hilolafayoziddinova14@gmail.com>
2024-02-13 17:54:17 +0500

    news page is done

--- src/routes/news/News.jsx
+++ src/routes/news/News.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from '../../data/yangiliklar.json'
+import { Button, Form, LabelInput } from '../../utils/Utils'
+import { Modal } from 'antd'
+import './News.scss'
 
 const News = () => {
+  console.log(data);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div>News</div>
+    <section className='news'>
+      <div className="actions">
+        <h2>Yangiliklar</h2>
+        <Button onClick={showModal}>+</Button>
+        <Modal className='modal' centered open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+          <h3>Yangiliklar</h3>
+          <Form>
+            <LabelInput text='Yangilik nomi' placeholder='Yangilik nomi' />
+            <LabelInput text='Izoh' placeholder='Izoh' />
+            <LabelInput text='Янгилик номи' placeholder='Янгилик номи' />
+            <LabelInput text='Изох' placeholder='Изох' />
+            <Button>Saqlash</Button>
+          </Form>
+        </Modal>
+      </div>
+      <div className="news__wrapper">
+        {
+        data?.map((news, index) =>
+          <div className="news__content" key={index}>
+            <h4>{news.title}</h4>
+            <p>{news.description}</p>
+          </div>
+        )
+      }
+      </div>
+    </section>
   )
 }
 
