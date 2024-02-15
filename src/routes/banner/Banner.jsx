bc4099055d428c53c71aa62936bed58318ad2e04
fhilola <hilolafayoziddinova14@gmail.com>
2024-02-15 06:52:17 +0500

    done

--- src/routes/banner/Banner.jsx
+++ src/routes/banner/Banner.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Form, LabelInput } from '../../utils/Utils'
+import { Modal, Button as AntButton, Flex } from 'antd'
+import bannerimage from '../../assets/banner.svg'
+import { FaRegTrashAlt } from "react-icons/fa";
+import './Banner.scss'
 
 const Banner = () => {
+   const [isModalOpen, setIsModalOpen] = useState(false);
+   const link = ['app_auth/55c16d85-009a-44b5-8f8e-134a9d2d269e/grant?authed_ids=1108357407251172327%2C1142043964366871832&desktop_protocol=figma&locale=en', 'app_auth/55c16d85-009a-44b5-8f8e-134a9d2d269e/grant?authed_ids=1108357407251172327%2C1142043964366871832&desktop_protocol=figma&locale=en', 'app_auth/55c16d85-009a-44b5-8f8e-134a9d2d269e/grant?authed_ids=1108357407251172327%2C1142043964366871832&desktop_protocol=figma&locale=en', 'app_auth/55c16d85-009a-44b5-8f8e-134a9d2d269e/grant?authed_ids=1108357407251172327%2C1142043964366871832&desktop_protocol=figma&locale=en', ]
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div>Banner</div>
+    <div className='banner'>
+      <div className="actions">
+        <h2>Banner</h2>
+        <Button onClick={showModal}>+</Button>
+        <Modal className='modal' centered open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+          <h3>Banner qo`shish</h3>
+          <Form>
+            <LabelInput text='Rasm Yuklash PNG, JPG (345x180px)' type='file' />
+            <LabelInput text='Url' placeholder='Url' />
+            <Button>Saqlash</Button>
+          </Form>
+        </Modal>
+      </div>
+      <div className="banner-content__wrapper">
+        {
+          link.map((item, index)=>
+          <div className="banner-content" key={index}>
+            <img src={bannerimage} alt="" />
+            <p>{item.slice(0, 32) + '...'}</p>
+            <AntButton style={{width: '100%'}} danger><FaRegTrashAlt/>O`chirish</AntButton>
+          </div>
+          )
+        }
+      </div>
+    </div>
   )
 }
 
--- src/routes/faq/Faq.jsx
+++ src/routes/faq/Faq.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form , Button, LabelInput} from '../../utils/Utils'
+import { Modal } from 'antd'
+import data from '../../data/yangiliklar.json'
 
 const Faq = () => {
+   const [isModalOpen, setIsModalOpen] = useState(false);
+   const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div>Faq</div>
+    <section className='faq'>
+      <div className="actions">
+        <h2>FAQ</h2>
+        <Button onClick={showModal}>+</Button>
+        <Modal className='modal' centered open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+          <h3>FAQ</h3>
+          <Form>
+            <LabelInput text='Savol nomi' placeholder='Savol nomi' />
+            <LabelInput text='Savolga jabvob' placeholder='Savolga jabvob' />
+            <LabelInput text='Савол номи' placeholder='Савол номи' />
+            <LabelInput text='Саволга жавоб' placeholder='Саволга жавоб' />
+            <Button>Saqlash</Button>
+          </Form>
+        </Modal>
+      </div>
+      <div className="news__wrapper">
+        {
+        data?.map((news, index) =>
+          <div className="news__content" key={index}>
+            <h4>{news.title}</h4>
+            <p>{news.description}</p>
+          </div>
+        )
+      }
+      </div>
+    </section>
   )
 }
 
